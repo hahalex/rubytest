12bd8b85612c57d9b0a95f4377dc975b96c92667
hahalex <at.hahalex@gmail.com>
2026-05-05 17:55:18 +0700

    снова l6

--- lesson6/qwiz.rb
+++ lesson6/qwiz.rb
@@ -7,26 +7,32 @@ require 'yaml'
 # откуда-то взять вопросы и ответы
 # YAML yet another markup language
 all_questions = YAML.safe_load(File.read('questions.yaml'), symbolize_names: true)
+correct_answer = []
 
 # брать каждый вопрос о очереди и предлагать 4 вараинат ответа
 all_questions.shuffle.each do |questions_data|
-  puts "\n\n=== #{questions_data[:question]} ===\n"
   
-  num = 1
-  correct_answer = questions_data[:answers][1]
-  questions_data[:answers].shuffle.each do |answer|
-    puts "#{num}. #{answer}"
-    num += 1
+    # puts questions_data[:answers].first.inspect
+    correct_answer << questions_data[:answers].first
+    puts correct_answer.inspect
+
+    puts "\n\n=== #{questions_data[:question]} ===\n"
+
+    num = 1
+    questions_data[:answers].shuffle.each do |answer|
+      puts "#{num}. #{answer}"
+      num += 1
+    end
+
+    puts correct_answer[(num-1)]
+    user_answer = gets.strip
+    if user_answer == correct_answer
+      puts "правильно"
+    else
+      puts "выберите вараинт ответа от 1 до 4"
+    end
   end
 
-  user_answer = gets.strip
-  puts correct_answer
-  if user_answer == correct_answer
-    puts "правильно"
-  else
-    puts "выберите вараинт ответа от 1 до 4"
-  end
-end
 
 #  юзер ввдит ответ с клавиатуры
 
