0f3ee40adcace97ed161740b134c1e3dc0cffee3
hahalex <at.hahalex@gmail.com>
2026-05-06 14:18:59 +0700

    l6 (my qwiz version)

--- lesson6/qwiz.rb
+++ lesson6/qwiz.rb
@@ -7,31 +7,49 @@ require 'yaml'
 # откуда-то взять вопросы и ответы
 # YAML yet another markup language
 all_questions = YAML.safe_load(File.read('questions.yaml'), symbolize_names: true)
-correct_answer = []
+correct_answers = []
+raw_questions = all_questions.shuffle
 
 # брать каждый вопрос о очереди и предлагать 4 вараинат ответа
-all_questions.shuffle.each do |questions_data|
+raw_questions.each do |questions_data|
   
-    # puts questions_data[:answers].first.inspect
-    correct_answer << questions_data[:answers].first
-    puts correct_answer.inspect
+  # puts questions_data[:answers].first.inspect
+  correct_answers << questions_data[:answers].first
+end
+puts correct_answers.inspect
 
-    puts "\n\n=== #{questions_data[:question]} ===\n"
+raw_questions.each do |questions_data|
+  puts "\n\n=== #{questions_data[:question]} ===\n"
 
-    num = 1
-    questions_data[:answers].shuffle.each do |answer|
-      puts "#{num}. #{answer}"
-      num += 1
-    end
+  num = 1
+  shuffle_answers = []
+  questions_data[:answers].shuffle.each do |answer|
+    shuffle_answers << answer
+    # puts shuffle_answers.inspect
+    puts "#{num}. #{answer}"
+    num += 1
+  end
 
-    puts correct_answer[(num-1)]
-    user_answer = gets.strip
-    if user_answer == correct_answer
-      puts "правильно"
+  # puts correct_answers.shift
+  # puts correct_answers.inspect
+
+  user_answer = nil
+
+  loop do
+    user_answer = gets.strip.to_i
+    if (1..4).include?(user_answer)
+      break
     else
       puts "выберите вараинт ответа от 1 до 4"
     end
   end
+
+  if (shuffle_answers[user_answer-1] == correct_answers.shift)
+    puts "правильно"
+  else
+    puts "неправильно"
+  end
+end
 
 
 #  юзер ввдит ответ с клавиатуры
